220258ff6f3ae031aeefdae32bf29c697d50f305
kornelia <kornelia@comp-nela>
2015-11-22 11:09:02 +0100

    first Commit

---
+++ Player.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author kornelia
+ */
+public enum Player {
+    PLAYER1("X"),
+    PLAYER2("O");
+    
+    private final String mark;
+    private Player(String mark){
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+    
+}
